f7db04367494ad83a5aca2d65bb141dedf5dedcd
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-02-08 21:09:01 +0300

    add file Replace With Alphabet Position.py

---
+++ Replace With Alphabet Position.py
@@ -0,0 +1,15 @@
+"""In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+
+If anything in the text isn't a letter, ignore it and don't return it.
+
+"a" = 1, "b" = 2, etc."""
+
+
+def alphabet_position(text):
+    s = ''
+    for i in text.lower():
+        if i.isalpha():
+            s += str(ord(i) - 96) + ' '
+        else:
+            continue
+    return s.strip()
