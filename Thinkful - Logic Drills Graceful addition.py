b78828894fb2f80b0644ee8fe65b2e6db01f8bcd
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-02-18 14:46:31 +0300

    add file Thinkful - Logic Drills Graceful addition.py

---
+++ Thinkful - Logic Drills Graceful addition.py
@@ -0,0 +1,17 @@
+"""You like the way the Python + operator easily handles adding different numeric types,
+but you need a tool to do that kind of addition without killing your program
+with a TypeError exception whenever you accidentally try adding incompatible types
+like strings and lists to numbers.
+
+You decide to write a function my_add() that takes two arguments.
+If the arguments can be added together it returns the sum.
+If adding the arguments together would raise an error the function should return None instead.
+For example, my_add(1, 3.414) would return 4.414, but my_add(42, " is the answer.") would return None.
+Hint: using a try / except statement may simplify this kata."""
+
+
+def my_add(a, b):
+    if type(a) == type(b) or (isinstance(a, (int, float)) and isinstance(b, (int, float))):
+        return a + b
+    else:
+        return None
