c5016046c9b66d6c38f201d9d90d8d5a8cb5ef63
JuliaKuznetsovaQA <kuznetsovajulia@gmail.com>
2024-02-25 01:57:36 +0300

    add file Building Spheres.py

---
+++ Building Spheres.py
@@ -0,0 +1,24 @@
+"""Now that we have a Block let's move on to something slightly more complex: a Sphere."""
+
+from math import pi
+
+class Sphere(object):
+    def __init__(self, radius, mass):
+        self.radius = radius
+        self.mass = mass
+
+    def get_radius(self):
+        return self.radius
+
+    def get_mass(self):
+        return self.mass
+
+    def get_volume(self):
+        return round(((4 / 3) * pi * (self.radius ** 3)), 5)
+
+    def get_surface_area(self):
+        return round((4 * pi * (self.radius ** 2)), 5)
+
+    def get_density(self):
+        return round((self.mass / ((4 / 3) * pi * (self.radius ** 3))), 5)
+
